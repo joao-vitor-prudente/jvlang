9e5f0c7911cb91ba7d0a6edc5af602ca51ff9a62
João Vitor Ceolin Prudente <jvitor.prudente@gmail.com>
2025-07-07 12:41:48 -0300

    feat: implement cli

---
+++ jvlang/CLI.cs
@@ -0,0 +1,30 @@
+namespace jvlang;
+
+internal class CliException(string message) : Exception(message);
+
+internal class FileNotProvidedException(string[] args)
+    : CliException($"No file provided. Args: {string.Join(", ", args)}");
+
+internal class ProvidedFileNotFoundException(string fileName)
+    : CliException($"Provided file not found. File: {fileName}");
+
+internal class ReadFileException(Exception cause)
+    : CliException($"Read file error. Cause: {cause}");
+
+public class Cli(string[] args)
+{
+    private string FileName => args.Length > 0 ? args[0] : throw new FileNotProvidedException(args);
+
+    public string ReadFile()
+    {
+        if (!File.Exists(FileName)) throw new ProvidedFileNotFoundException(FileName);
+        try
+        {
+            return File.ReadAllText(FileName);
+        }
+        catch (Exception e)
+        {
+            throw new ReadFileException(e);
+        }
+    }
+}
--- jvlang/Program.cs
+++ jvlang/Program.cs
@@ -4,5 +4,8 @@ public class Program
 {
     public static void Main(string[] args)
     {
+        var cli = new Cli(args);
+        var text = cli.ReadFile();
+        Console.WriteLine(text);
     }
 }
